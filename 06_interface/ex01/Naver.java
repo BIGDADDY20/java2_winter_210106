2fe0f7c0741c294d6a7db407721cb53008879e58
marobiana <69413030+marobiana@users.noreply.github.com>
2021-01-21 11:49:43 +0900

    Update Naver.java

---
+++ 06_interface/ex01/Naver.java
@@ -0,0 +1,14 @@
+package interface_ex01;
+
+public class Naver {
+	// 자바 프로그래머 채용
+	public void recruitJavaProgrammer(JavaProgramming java) {
+		// java 기능이 있는 사람을 뽑고, 그 사람의 지식을 본다.
+		java.objectOriented();
+	}
+	
+	// C 프로그래머 채용
+	public void recruitCProgrammer(CProgramming c) {
+		c.procedure();
+	}
+}
