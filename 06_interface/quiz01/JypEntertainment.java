b653d87d128b5f2188de3411ecef591187921d79
marobiana <69413030+marobiana@users.noreply.github.com>
2021-01-21 13:23:54 +0900

    Update JypEntertainment.java

---
+++ 06_interface/quiz01/JypEntertainment.java
@@ -0,0 +1,13 @@
+package interface_quiz01;
+
+public class JypEntertainment {
+	// 배우 오디션
+	public void actorAudition(Act act) {
+		act.action();
+	}
+	
+	// 아이돌 오디션
+	public void idolAudition(Sing sing) {
+		sing.song();
+	}
+}
